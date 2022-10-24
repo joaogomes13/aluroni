dbf85184abef02e47393552cfeec256bf4c2a390
JuaumMitty <jjoaovictorpg@gmail.com>
2022-10-24 06:46:21 -0300

    Criando sobre

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,18 @@ import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-
         <Routes>
           <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
       </Router>
